39f16ab735713e2640c1c69d25925ed8df8f8cf6
vedchaudhari <vedc2853@gmail.com>
2024-08-23 15:38:19 +0530

    Socket url update

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -26,25 +26,3 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 
 export default GenderCheckbox;
 
-
-// const GenderRadio = () => {
-//     return (
-//         <div className="flex">
-//             <div className="form-control">
-//                 <label className="label gap-2 cursor-pointer">
-//                     <span className="label-text">Male</span>
-//                     <input type="radio" name="gender" className="radio border-slate-900" value="male" />
-//                 </label>
-//             </div>
-
-//             <div className="form-control">
-//                 <label className="label gap-2 cursor-pointer">
-//                     <span className="label-text">Female</span>
-//                     <input type="radio" name="gender" className="radio border-slate-900" value="female" />
-//                 </label>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default GenderRadio;
